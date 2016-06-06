e334dcc6ead321136e2f8208a0f01de48e5e5d8c
M. Alonso <port02.server@gmail.com>
2016-06-06 16:01:34 +0100

    initial commit v0.1.0

---
+++ address.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+#    GNU Condo: The Free Management Condominium System
+#    Copyright (C) 2016- M. Alonso <port02.server@gmail.com>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval, If, Bool
+
+
+__all__ = ['Address']
+__metaclass__ = PoolMeta
+
+
+class Address:
+    "Address"
+    __name__ = 'party.address'
+
+    @classmethod
+    def __setup__(cls):
+        super(Address, cls).__setup__()
+        cls.subdivision.domain.append(If(Bool(Eval('zip')), [('zips.zip', '=', Eval('zip'))],[]))
+
+    @staticmethod
+    def default_country():
+        Configuration = Pool().get('party.configuration')
+        config = Configuration(1)
+        if config.default_country:
+            return config.default_country.id
---
+++ country.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+#    GNU Condo: The Free Management Condominium System
+#    Copyright (C) 2016- M. Alonso <port02.server@gmail.com>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+
+__all__ = ['Subdivision']
+__metaclass__ = PoolMeta
+
+
+class Subdivision:
+    "Subdivision"
+    __name__ = 'country.subdivision'
+
+    zips = fields.One2Many('country.zip', 'subdivision', 'Zips')
